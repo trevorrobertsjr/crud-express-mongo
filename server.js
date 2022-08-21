55c47c7debcd7bc07b5ed5d88f5f0aa596a6ac9f
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2022-08-21 01:14:52 -0400

    CRUD complete. next, improve UI

--- server.js
+++ server.js
@@ -18,23 +18,35 @@ MongoClient.connect(connectionString)
         const db = client.db('star-wars-quotes');
         const quotesCollection = db.collection('quotes');
 
+        // Templating Engine: EJS
+        app.set('view engine', 'ejs')
+
         // Make sure you place body-parser before your CRUD handlers!
         app.use(bodyParser.urlencoded({ extended: true }))
+
+        // Accommodate JSON
+        app.use(bodyParser.json())
+
+        // clientside javascript, css, etc.
+        app.use(express.static('public'))
 
         // Define routes
         app.get('/', (req, res) => {
             const cursor = db.collection('quotes').find().toArray()
                 .then(results => {
-                    console.log(results)
+                    // console.log(results)
+                    res.render('index.ejs', { quotes: results })
                 })
                 .catch(error => console.error(error))
-            console.log(cursor);
+            // console.log(cursor);
             // res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.sendFile(__dirname + '/index.html');
             // res.end();
+            // res.sendFile(__dirname + '/index.html');
+
 
         })
 
+        // Insert new quotes
         app.post('/quotes', (req, res) => {
             quotesCollection.insertOne(req.body)
                 .then(result => {
@@ -43,6 +55,48 @@ MongoClient.connect(connectionString)
                 })
                 .catch(error => console.error(error))
         })
+
+        // Update existing quotes
+        app.put('/quotes', (req, res) => {
+            console.log(req.body)
+            quotesCollection.findOneAndUpdate(
+                { name: 'Yoda' },
+                {
+                    $set: {
+                        name: req.body.name,
+                        quote: req.body.quote
+                    }
+                },
+                {
+                    upsert: true
+                }
+            )
+                .then(result => {
+                    // console.log(result)
+                    res.json('Success')
+                })
+                .catch(error => console.error(error))
+        })
+        // Delete Darth Vader quotes
+        app.delete('/quotes', (req, res) => {
+            // console.log(req.body.name)
+            // quotesCollection.findOneAndDelete(
+            quotesCollection.deleteOne(
+                // { name: 'Darth Vader' }
+                { name: req.body.name }
+            )
+                .then(result => {
+                    if (result.deletedCount === 0) {
+                        // console.log('go back')
+                        return res.json('No quote to delete')
+
+                    }
+                    // console.log(result)
+                    res.json(`Deleted Darth Vader's quote`)
+                })
+                .catch(error => console.error(error))
+        })
+
 
         // Start web server
         app.listen(PORT, () => {
